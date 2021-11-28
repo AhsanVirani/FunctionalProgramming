00e3b3c785f47c479f3c981acafc1f7c3ca4ede9
ahsan <ahsan.virani08@gmail.com>
2021-11-29 00:19:35 +1100

    listFunc

--- week1/listFunctions.hs
+++ week1/listFunctions.hs
@@ -1,4 +1,5 @@
 import Data.Char (toLower)
+
 -- Creating custom map function
 -- Base case when list is empty
 -- When list is non empty
@@ -6,3 +7,31 @@ import Data.Char (toLower)
 myMap :: (a -> b) -> [a] -> [b]
 myMap f [] = []
 myMap f (x:xs) = f x : myMap f xs
+
+-- sum
+mySum :: [Int] -> Int 
+mySum []     = 0
+mySum (x:xs) = x + mySum xs
+
+myConcat :: [[a]] -> [a]
+myConcat [] = []
+myConcat (x:xs) = x ++ myConcat xs
+
+-- abstract away recursion
+myFoldr :: (a -> b -> b) -> b -> [a] -> b
+myFoldr f z [] = z
+myFoldr f z (x:xs) = x `f` (myFoldr f z xs) 
+
+mySum' = myFoldr (+) 0
+myConcat' = myFoldr (++) []
+
+-- filter
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter p [] = []
+myFilter p (x:xs) = if p x then x : myFilter p xs
+                           else  myFilter p xs
+
+myFilter' :: (a -> Bool) -> [a] -> [a]
+myFilter' p (x:xs) 
+    | p x       = x : myFilter' p xs 
+    | otherwise = myFilter' p xs
