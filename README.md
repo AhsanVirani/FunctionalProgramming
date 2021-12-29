e9755c68583b80635070940a8c4935b153cd26f2
ahsan <ahsan.virani08@gmail.com>
2021-12-29 15:19:55 +1100

    Read

---
+++ README.md
@@ -0,0 +1,28 @@
+# Functional Programming in Haskell
+
+## Week 1:
+* Introduction to Functional
+* Syntax
+* First Program
+* Second Program
+
+## Week 2:
+* Filter
+* Mapping
+* Foldr & Foldl
+* Datatypes
+* Pattern Matching
+* Maybe
+* Typeclass
+* Ex1 (UNSW)
+
+## Week3:
+* Binary Tree
+* Either
+* Maybe in detail
+* Typeclass in detail
+
+## Week 4:
+* Monoid
+* Functors
+
--- week4/Monoid.hs
+++ week4/Monoid.hs
@@ -3,8 +3,27 @@
     -- There is a default or empty value of the data type.
     -- Given two values of the data type, they can be combined to create a single value.
 
+-- Defining Option datatype
+data Option = Option { boolOption :: Bool, selections :: [String]}
+    deriving Show
+
+
+-- -- Defining instance of monoid
+-- instance Semigroup Options => Monoid Option
+--     where
+--         mempty = Option False []
+--         (Option b1 s1) `mappend` (Option b2 s2) = Option (b1 || b2) (s1 ++ s2)
+
+-- main :: IO ()
+-- main do 
+--     putStrLn "Define default options"
+--     let defaultOptions = mempty :: Option
+--     putStrLn (show defaultOptions)
+
 -- Monoid Class Definition
-class Monoid a where
-    mempty  :: a
-    mappend :: a -> a -> a
-    mconcat :: [a] -> a
+-- class Monoid a where
+--     mempty  :: a
+--     mappend :: a -> a -> a
+--     mconcat :: [a] -> a
+
+
